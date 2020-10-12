26dc806a6b4b5b08fe38719f526f8ac3c75d0fe2
Bhavana Nagabandi <Bhavana.Nagabandi@walmartlabs.com>
2020-10-12 16:54:44 -0700

    songsList app using graphQL and Apollo client

--- client/index.js
+++ client/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "./style/style.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+
+import App from "./components/App";
+import SongsList from "./components/SongsList";
+import SongCreate from "./components/SongCreate";
+
+const client = new ApolloClient();
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongsList} />
+          <Route path="songs/new" component={SongCreate} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
